fe9c78a26e71d5793947d0a0db24435c67ac7a8c
Addyant <addyant09@gmail.com>
2025-09-15 22:43:27 +0530

    Adding logic to ball and player

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool IsPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+    private float movement;
+    
+
+    void Update()
+    {
+        if (IsPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+        rb.linearVelocity = new Vector2(0, movement*speed);
+    }
+}
---
+++ Assets/direction.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class direction : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.linearVelocity = new Vector2(speed * x, speed * y);
+    }
+}
